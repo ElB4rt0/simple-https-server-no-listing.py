adb7dde4b5d7b541964e4c0291d317fc36b6f6a8
Elb4rt0 <37635891+ElB4rt0@users.noreply.github.com>
2020-06-23 17:50:38 -0400

    Create simple-https-server-no-listing.py

---
+++ simple-https-server-no-listing.py
@@ -0,0 +1,18 @@
+
+#!/usr/bin/env python3
+import http.server
+import socketserver
+import ssl
+
+class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.path = 'Index.html'
+        return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+# Create an object of the above class
+handler_object = MyHttpRequestHandler
+
+httpd = http.server.HTTPServer(('0.0.0.0', 443), handler_object)
+httpd.socket = ssl.wrap_socket(httpd.socket, certfile='./ssl/cert.pem', keyfile='./ssl/key.pem', server_side=True, ssl_version=ssl.PROTOCOL_TLSv1_2)
+httpd.serve_forever()
